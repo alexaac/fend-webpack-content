a2b6d1d16857a8b85a355bf63fb155196a2216c5
Your Name <you@example.com>
2021-07-21 15:54:23 +0300

    Add Webpack config

--- src/server/index.js
+++ src/server/index.js
@@ -13,8 +13,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(7777, function () {
+    console.log('Example app listening on port 7777!')
 })
 
 app.get('/test', function (req, res) {

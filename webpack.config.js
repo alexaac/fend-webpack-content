e9677af65911d4781224cb12b096fb8b97cd9572
Your Name <you@example.com>
2021-07-24 19:17:03 +0300

    Add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,15 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
+  module: {
+    rules: [
+      {
+        test: '/.js$/',
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/client/views/index.html',
